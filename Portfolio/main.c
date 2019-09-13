8ddc0f907c4d45da1957fc8b6eda976bb0f3dc7f
Ak Prathipati <akprathipati@gmail.com>
2019-09-13 14:29:50 -0700

    Initial Commit

---
+++ Portfolio/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Portfolio
+//
+//  Created by Prathipati, Akshit on 9/13/19.
+//  Copyright © 2019 Prathipati, Akshit. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
